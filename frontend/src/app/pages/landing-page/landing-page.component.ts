9b44dcf338acb82f5fc65436dd880078916911a4
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-12 14:50:18 +0200

    landing page component ready to get data

--- frontend/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IFriendViewModel } from 'src/app/core/models/IFriendViewModel';
+import { FriendService } from 'src/app/core/services/friend-service/friend.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  friendData!: IFriendViewModel;
+  friendSubscription!: Subscription;
 
-  constructor() { }
+  friends: IFriendViewModel[] = [];
+
+  constructor(private friendService: FriendService) {}
 
   ngOnInit(): void {
+    this.friendService.getAllFriends();
+    this.friendSubscription = this.friendService.friendsObservable$.subscribe(
+      (observableResponse: IFriendViewModel[]) => {
+        this.friends = observableResponse;
+      }
+    );
   }
-
 }
