bc06e6b6fdee307de51b27d84d7516c8098e3ee3
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-12 14:29:32 +0200

    service getAllFriend method first version

--- frontend/src/app/core/services/friend-service/friend.service.ts
+++ frontend/src/app/core/services/friend-service/friend.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IFriendViewModel } from '../../models/IFriendViewModel';
 
 @Injectable({
@@ -12,4 +13,12 @@ export class FriendService {
     this.friendsSubject.asObservable();
 
   constructor(private http: HttpClient) {}
+
+  getAllFriends(): void {
+    this.http
+      .get<IFriendViewModel[]>(`${environment.baseUrl}/Friend`)
+      .subscribe((friends: IFriendViewModel[]) => {
+        this.friendsSubject.next(friends);
+      });
+  }
 }
