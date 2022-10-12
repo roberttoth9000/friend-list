b65ef48507ac86fc18629fb47f97698134798ddf
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-12 15:56:11 +0200

    friend shared component created

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { FriendComponent } from './components/friend/friend.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FriendComponent],
   imports: [CommonModule, RouterModule],
   providers: [],
   exports: [HeaderComponent],
