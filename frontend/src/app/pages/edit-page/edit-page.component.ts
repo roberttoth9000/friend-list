5797de658150d68b7fb53d00e19a61fd6358c0ac
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-13 19:18:20 +0200

    start formgroup to add new friend

--- frontend/src/app/pages/edit-page/edit-page.component.ts
+++ frontend/src/app/pages/edit-page/edit-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+  styleUrls: ['./edit-page.component.scss'],
 })
 export class EditPageComponent implements OnInit {
+  addNewFriendForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.addNewFriendForm.value);
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
