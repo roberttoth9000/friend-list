79e4e863bd9ad1b2f3648c8a3a792676ba266f85
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-13 10:36:24 +0200

    fix import issues

--- frontend/src/app/pages/landing-page/landing-page.module.ts
+++ frontend/src/app/pages/landing-page/landing-page.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
-
 import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [CommonModule, LandingPageRoutingModule],
+  imports: [CommonModule, LandingPageRoutingModule, SharedModule],
 })
 export class LandingPageModule {}
