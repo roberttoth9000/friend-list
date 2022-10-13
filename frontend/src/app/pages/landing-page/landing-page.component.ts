e837ee216b584ff8eb0ce0772dbf7225b5deeff2
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-13 10:41:00 +0200

    ngFor test with dummy data

--- frontend/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -11,16 +11,41 @@ import { FriendService } from 'src/app/core/services/friend-service/friend.servi
 export class LandingPageComponent implements OnInit {
   friendSubscription!: Subscription;
 
-  friendsList: IFriendViewModel[] = [];
+  friendsList: IFriendViewModel[] = [
+    {
+      id: 1,
+      name: 'Sanyi',
+      email: 'test1@gmail.com',
+      comment: 'Az első teszt komment',
+      favFoodname: 'lasagne',
+      relationshipStatus: 2,
+    },
+    {
+      id: 2,
+      name: 'Éva',
+      email: 'test2@gmail.com',
+      comment: 'A második teszt komment',
+      favFoodname: 'sültcsirke',
+      relationshipStatus: 2,
+    },
+    {
+      id: 1,
+      name: 'Barnabás',
+      email: 'barnabas42@gmail.com',
+      comment: 'Itt van még egy teszt komment',
+      favFoodname: 'nincs',
+      relationshipStatus: 0,
+    },
+  ];
 
   constructor(private friendService: FriendService) {}
 
   ngOnInit(): void {
     this.friendService.getAllFriends();
-    this.friendSubscription = this.friendService.friendsObservable$.subscribe(
-      (observableResponse: IFriendViewModel[]) => {
-        this.friendsList = observableResponse;
-      }
-    );
+    // this.friendSubscription = this.friendService.friendsObservable$.subscribe(
+    //   (observableResponse: IFriendViewModel[]) => {
+    //     this.friendsList = observableResponse;
+    //   }
+    // );
   }
 }
