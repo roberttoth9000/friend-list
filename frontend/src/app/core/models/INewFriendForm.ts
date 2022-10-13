651508eb0349788345288f159981e970f1460353
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-13 21:32:34 +0200

    edit page html, component first version

---
+++ frontend/src/app/core/models/INewFriendForm.ts
@@ -0,0 +1,7 @@
+export interface INewFriendForm {
+  name: string;
+  email: string;
+  comment: string;
+  favFood: string;
+  relationshipStatus: string;
+}
--- frontend/src/app/pages/edit-page/edit-page.component.ts
+++ frontend/src/app/pages/edit-page/edit-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { INewFriendForm } from 'src/app/core/models/INewFriendForm';
+import { FriendService } from 'src/app/core/services/friend-service/friend.service';
 
 @Component({
   selector: 'app-edit-page',
@@ -9,12 +11,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class EditPageComponent implements OnInit {
   addNewFriendForm = new FormGroup({
     name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    comment: new FormControl(''),
+    favFood: new FormControl('', Validators.required),
+    relationshipStatus: new FormControl('', Validators.required),
   });
 
   onSubmit() {
-    console.log(this.addNewFriendForm.value);
+    const newFriendForm: INewFriendForm = this.addNewFriendForm.value;
+    if (this.addNewFriendForm.valid) {
+      this.friendService.addNewFriend(newFriendForm);
+    }
   }
-  constructor() {}
+  constructor(private friendService: FriendService) {}
 
   ngOnInit(): void {}
 }
