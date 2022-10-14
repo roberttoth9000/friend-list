c9f895e398a9c960c9ae1c62002be6e4ce53da6b
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-14 13:44:15 +0200

    food service getAllFood method

--- frontend/src/app/core/services/food-service/food.service.ts
+++ frontend/src/app/core/services/food-service/food.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IFood } from '../../models/IFood';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
+  private foodsSubject = new BehaviorSubject<IFood[]>([]);
+  foodsObservable$: Observable<IFood[]> = this.foodsSubject.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllFood(): void {
+    this.http
+      .get<IFood[]>(`${environment.baseUrl}/Food`)
+      .subscribe((foods: IFood[]) => {
+        this.foodsSubject.next(foods);
+      });
+  }
 }
