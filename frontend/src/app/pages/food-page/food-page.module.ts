8257af40db92a35ec75a2535cb42623e24c8a30d
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-14 20:55:22 +0200

    food-pages modules done

--- frontend/src/app/pages/food-page/food-page.module.ts
+++ frontend/src/app/pages/food-page/food-page.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FoodPageRoutingModule } from './food-page-routing.module';
-
+import { FoodPageComponent } from './food-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [FoodPageComponent],
   imports: [
     CommonModule,
-    FoodPageRoutingModule
-  ]
+    FoodPageRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
-export class FoodPageModule { }
+export class FoodPageModule {}
