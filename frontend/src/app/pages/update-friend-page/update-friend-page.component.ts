4b94191d38e7673f0ab98d0be615a0a5b5ff5b5d
roberttoth9000 <roberttoth9000@gmail.com>
2022-10-15 10:38:14 +0200

    update page component generated

---
+++ frontend/src/app/pages/update-friend-page/update-friend-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-friend-page',
+  templateUrl: './update-friend-page.component.html',
+  styleUrls: ['./update-friend-page.component.scss']
+})
+export class UpdateFriendPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
